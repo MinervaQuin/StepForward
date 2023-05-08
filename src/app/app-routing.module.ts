3b20b890a53d5f1e087a55461935c00283879c9c
Raul Medina <raul.medina@bitingbit.com>
2023-05-08 11:45:20 +0100

    Feature: User registration - Set up AngularFire and user registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotLoggedMainPageComponent } from './not-logged-main-page/not-logged-main-page.component';
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
 
 const routes: Routes = [
-  { path: '', component: NotLoggedMainPageComponent }
+  { path: '', component: NotLoggedMainPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotLoggedMainPageComponent } from './not-logged-main-page/not-logged-main-page.component';
 import { NgbModule, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { LoginPageComponent } from './login-page/login-page.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotLoggedMainPageComponent
+    NotLoggedMainPageComponent,
+    RegisterPageComponent,
+    LoginPageComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    NgbCarouselModule
+    NgbCarouselModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]

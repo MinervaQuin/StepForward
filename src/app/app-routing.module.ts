6353070970fcd92d2e74748704b8be3be906c6dd
Minerva <minervaqf8@gmail.com>
2023-05-11 22:35:57 +0100

    Page: Rewards - creating component, changing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotLoggedMainPageComponent } from './not-logged-main-page/not-logged-main-page.component';
+import { RewardsPageComponent } from './rewards-page/rewards-page.component';
 
 const routes: Routes = [
   { path: '', component: NotLoggedMainPageComponent }
+  ,
+  { path: 'rewards', component: RewardsPageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotLoggedMainPageComponent } from './not-logged-main-page/not-logged-main-page.component';
 import { NgbModule, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { RewardsPageComponent } from './rewards-page/rewards-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotLoggedMainPageComponent
+    NotLoggedMainPageComponent,
+    RewardsPageComponent
   ],
   imports: [
     BrowserModule,
